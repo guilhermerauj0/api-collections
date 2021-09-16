ff619620e5723752ea9c89e53b1367e7407aac81
arthu <arthurguilhermesb@gmail.com>
2021-09-16 14:50:52 -0300

    using arrayOf e and array for strings

---
+++ src/main/kotlin/me/arthu/collections/TesteArrayString.kt
@@ -0,0 +1,19 @@
+package me.arthu.collections
+
+fun main(){
+    val nomes = Array(3){""}
+    nomes[0] = "MArta"
+    nomes[1] = "Zazá"
+    nomes[2] = "Jose"
+
+    for(nome in nomes){ println(nome) }
+
+    println("-----------------------")
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    val nomes2 = arrayOf("Maria", "Zazá", "Pedro")
+    println("-----------------------")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
