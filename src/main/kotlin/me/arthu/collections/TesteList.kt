bf912d2a5f2f42e8794d1b0b47809e52415d992f
arthu <arthurguilhermesb@gmail.com>
2021-09-17 15:48:42 -0300

    operations in list with listOf, groupBy and sortedBy

---
+++ src/main/kotlin/me/arthu/collections/TesteList.kt
@@ -0,0 +1,43 @@
+package me.arthu.collections
+
+fun main(){
+    val joao = Funcionario("João", 2400.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+
+    println("------------------------------------")
+    println(funcionarios.find{it.nome == "Maria"})
+
+
+    println("------------------------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+
+    println("------------------------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+
+){
+    override fun toString(): String =
+        """
+            Nome:       $nome
+            Salario:    $salario
+        """.trimMargin()
+
+}
+
